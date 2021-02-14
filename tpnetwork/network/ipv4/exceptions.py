5bf4656142f7f69c2b1faddfb8e143b32dd57ad9
Thomas Pajon <th.pajon45@gmail.com>
2021-02-14 23:20:09 +0100

    Add ipv4 network exceptions

---
+++ tpnetwork/network/ipv4/exceptions.py
@@ -0,0 +1,15 @@
+class AddressValueError(ValueError):
+    def __init__(self, address, message='bad ipv4 address value'):
+        self._address = address
+        self._message = message
+        super().__init__(self._message)
+    def __str__(self):
+        return f'{self._message} : {self._address}'
+
+class NetmaskValueError(ValueError):
+    def __init__(self, netmask, message='bad ipv4 netmask value'):
+        self._netmask = netmask
+        self._message = message
+        super().__init__(self._message)
+    def __str__(self):
+        return f'{self._message} : {self._netmask}'
