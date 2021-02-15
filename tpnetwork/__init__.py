c103c1620d1fe72834cbfb7dcb2487e8160a16c5
Thomas Pajon <th.pajon45@gmail.com>
2021-02-15 19:21:59 +0100

    Add modules dependencies

---
+++ tpnetwork/__init__.py
@@ -0,0 +1,1 @@
+from .network import IPv4Address, IPv4Network, binary_to_ipv4, AddressValueError, NetworkValueError
---
+++ tpnetwork/network/__init__.py
@@ -0,0 +1,1 @@
+from .ipv4 import IPv4Address, IPv4Network, binary_to_ipv4, AddressValueError, NetworkValueError
