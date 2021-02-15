58a80372b5a833a69eddf091b49e430f924c8abe
Thomas Pajon <th.pajon45@gmail.com>
2021-02-15 22:03:35 +0100

    Add requirements.txt

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setuptools.setup(
     name='tpnetwork',
-    version='1.1.1',
+    version='1.1.2',
     author='Thomas Pajon',
     author_email='th.pajon45@gmail.com',
     description='A python network module',
