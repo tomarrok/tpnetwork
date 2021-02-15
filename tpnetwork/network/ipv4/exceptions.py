4547035061c694192e36f54735a9c1e1f8953f67
Thomas Pajon <th.pajon45@gmail.com>
2021-02-15 15:18:28 +0100

    Delete netmask value exception

--- tpnetwork/network/ipv4/exceptions.py
+++ tpnetwork/network/ipv4/exceptions.py
@@ -6,14 +6,6 @@ class AddressValueError(ValueError):
     def __str__(self):
         return f'{self._message} : {self._address}'
 
-class NetmaskValueError(ValueError):
-    def __init__(self, netmask, message='bad ipv4 netmask value'):
-        self._netmask = netmask
-        self._message = message
-        super().__init__(self._message)
-    def __str__(self):
-        return f'{self._message} : {self._netmask}'
-
 class NetworkValueError(ValueError):
     def __init__(self, network, message='bad ipv4 network value'):
         self._network = network
