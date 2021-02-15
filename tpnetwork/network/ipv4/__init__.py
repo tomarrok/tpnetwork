527f08b0fd967e9ddab547a182dbf9468b4ac041
Thomas Pajon <th.pajon45@gmail.com>
2021-02-15 15:56:11 +0100

    Patch additions

--- tpnetwork/network/ipv4/__init__.py
+++ tpnetwork/network/ipv4/__init__.py
@@ -1,3 +1,3 @@
-from .exceptions import AddressValueError, NetmaskValueError, NetworkValueError
+from .exceptions import AddressValueError, NetworkValueError
 from .address import IPv4Address, binary_to_ipv4
 from .network import IPv4Network
